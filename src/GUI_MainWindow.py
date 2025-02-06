f7b53e126f0ef0825e9d56f8498776ef3afb9642
Abdelrhman Eldenary <abdelrhmaneldenary@gmail.com>
2025-02-06 12:14:41 +0200

    added on mainWindow file and the rosNode file

--- src/GUI_MainWindow.py
+++ src/GUI_MainWindow.py
@@ -1,5 +1,9 @@
-from PyQt5.QtWidgets import QWidget, QLabel
-from PyQt5.QtGui import QIcon
+import rclpy
+from GUI_ROSNode import GUI_ROSNode
+from PyQt5.QtWidgets import QWidget, QLabel,QHBoxLayout
+from PyQt5.QtGui import QIcon,QPushButton
+from PyQt5.QtCore import QThread, pyqtSignal
+
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -13,6 +17,35 @@ class MainWindow(QWidget):
         self.initLabels()
 
     def initLabels(self):
-        label = QLabel(self)
-        label.setText("I am here")
-        
+        self.label = QLabel(self)
+        self.label.setText("I am here")
+        self.reset_button=QPushButton("reset",self)
+        self.reset_button.clicked.connect(self.reset_data)
+        layout = QHBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.reset_button)
+        self.setLayout(layout)
+
+
+        self.ros_thread = ROS2Thread()
+        self.ros_thread.signal_received.connect(self.update_label)  
+        self.ros_thread.start()
+    
+    def reset_data(self):
+        self.label.setText("data reseted")
+    
+    def update_label(self, message):
+        self.label.setText(f"{message}")
+
+
+class ROS2Thread(QThread):    
+    signal_received = pyqtSignal(str)  
+
+    def run(self):
+        """Run ROS 2 node in a separate thread."""
+        rclpy.init()
+        self.node = GUI_ROSNode()
+        self.node.data_received.connect(self.signal_received.emit)  
+        rclpy.spin(self.node)  
+        self.node.destroy_node()
+        rclpy.shutdown()
---
+++ src/GUI_ROSNode.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from PyQt6.QtCore import QObject, pyqtSignal
+
+
+class GUI_ROSNode(Node,QObject):
+
+    data_received=pyqtSignal(str)
+    def __init__(self):
+        Node.__init__('GUI_ROSNode')
+        QObject.__init__(self)
+        self.subscription = self.create_subscription(
+            String,
+            'topic',
+            self.listener_callback,
+            10)
+
+    def listener_callback(self, msg):
+            self.data_received.emit(msg.data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
